01eb5e88d5f834a4e0f1ce2ad26cf852d9230855
“shubh-ummm” <shubhamn2717@gmail.com>
2024-05-25 16:11:26 +0530

    create and run server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUP from "./pages/SignUP";
+import Headers from "./components/Headers";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <BrowserRouter>
+    <Headers/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/profile" element={<Profile />}/>
+        <Route path="/signin" element={<SignIn />}/>
+        <Route path="/signup" element={<SignUP />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
